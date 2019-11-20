93a67068d884b44de70e47ce8bd2d7f1df086797
patriciakasapa <patriciakasapa07@gmail.com>
2019-11-20 13:55:21 +0000

    New Spring

---
+++ Spring-Web2/src/test/java/io/turntabl/SpringWeb2/SpringWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.SpringWeb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
